2f2ce609505d5d177dbbc07c31ab9f1c944e16b0
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-06-26 13:05:44 -0300

    Thanks God

---
+++ Day 41/app.js
@@ -0,0 +1,33 @@
+// while(condição) {
+
+// }
+
+// let numero = 1;
+
+// while(numero <= 10) {
+//   console.log(numero);
+//   numero++;
+// }
+
+// let numero = 10;
+
+// while(numero >= 1) {
+//   console.log(numero);
+//   numero--;
+// }
+
+// let numero = 1;
+// while(numero <= 10) console.log(numero++);
+
+// let numero = 1;
+// do {
+//   console.log(numero);
+//   numero++;
+// } while(numero <= 10);
+
+let numero = 1;
+do {
+  console.log(numero);
+  numero++;
+  if (numero > 6) break;
+} while (numero <= 10);
