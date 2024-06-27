f353c6e216d23a2f8bb2daab6e793b9d3fa34847
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-06-27 18:21:35 -0300

    Thanks God

--- Day 42/app.js
+++ Day 42/app.js
@@ -1,9 +1,41 @@
-// for(início; condição; step){
+// for(início; cindição; step) {
 //   ações
 // }
 
-// for(let numero = 1; numero <= 10; numero++){
+// for(let numero = 1; numero <= 10; numero++) {
 //   console.log(numero);
 // }
 
-console.log(10 < 1)
+// let numero = 1;
+// for(;numero <= 10; numero++) {
+//   console.log(numero);
+// }
+
+// let numero = 1;
+// for(;numero <= 10;) {
+//   console.log(numero++);
+// }´
+
+// for(let numero = 1; numero <= 10; numero++) {
+//   if(numero == 5) continue;
+//   console.log(numero);
+// }
+
+// for(let numero = 1; numero <= 10; numero++) {
+//   console.log(numero);
+//   if(numero == 5) break;
+// }
+
+// for(let valor1 = 1; valor1 <= 4; valor1++){
+//   for(let valor2 = 1; valor2 <= 3; valor2++){
+//     console.log(`${valor1} X ${valor2} = ${valor1 * valor2}`);
+//   }
+// }
+
+ciclo: for(let valor1 = 1; valor1 <= 4; valor1++){
+  for(let valor2 = 1; valor2 <= 3; valor2++){
+    if(valor1 * valor2 == 6) break ciclo;
+    console.log(`${valor1} X ${valor2} = ${valor1 * valor2}`);
+  }
+}
+
